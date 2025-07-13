5b96a0fd7ba96c88fcdd490730f7f93c24e36a92
Satya-bit <satya.s@ahduni.edu.in>
2025-07-13 11:35:59 -0500

    Fourth commit

--- agent/round_manager.py
+++ agent/round_manager.py
@@ -1,7 +1,7 @@
 # This includes the available rounds to select before starting the interview.
 # Defines available rounds
 AVAILABLE_ROUNDS = {
-    "1": {"name": "HR", "num_questions": 3},
+    "1": {"name": "HR", "num_questions": 2},
     "2": {"name": "Technical", "num_questions": 5},
     "3": {"name": "Managerial", "num_questions": 4},
     "4": {"name": "General", "num_questions": 5} # Added General round
